8a381b3b715786c5d18b3c40756a2b1f98e81792
Agnija <ilzena.agnija@gmail.com>
2021-12-29 18:46:13 +0100

    adding required files of ex3

--- public/details.php
+++ public/details.php
@@ -6,4 +6,4 @@ require_once __DIR__ . '/../config/twig.php';
 
 $name = 'Zoe';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('details.html.twig', ['name' => $name]);
